9e5d520d296be8344a9d94a9e66050676b6c0ca0
Neil Gebhard <neilgebhard@gmail.com>
2022-03-14 22:35:37 +1000

    :lipstick: Fix alignment issue

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import { MdEmail } from "react-icons/md";
 export default function Footer() {
   return (
     <footer className="flex flex-col items-center justify-center py-32 bg-gray-100 space-y-5">
-      <ul className="list-none flex items-center justify-center gap-x-4">
+      <ul className="p-0 m-0 list-none flex items-center justify-center gap-x-4">
         <li>
           <a
             href="https://github.com/neilgebhard"
