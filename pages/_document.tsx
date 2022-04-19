9863004910a7475b7d96fefe38f314607fec0cb1
Neil Gebhard <neilgebhard@gmail.com>
2022-04-19 15:47:34 +1000

    :technologist: Add favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,25 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Roboto+Mono:wght@400;700&display=swap"
             rel="stylesheet"
           />
-          <link rel="shortcut icon" href="/favicon.ico" />
+          <link href="/favicons/favicon.ico" rel="shortcut icon" />
+          <link href="/favicons/site.webmanifest" rel="manifest" />
+          <link
+            href="/favicons/apple-touch-icon.png"
+            rel="apple-touch-icon"
+            sizes="180x180"
+          />
+          <link
+            href="/favicons/favicon-32x32.png"
+            rel="icon"
+            sizes="32x32"
+            type="image/png"
+          />
+          <link
+            href="/favicons/favicon-16x16.png"
+            rel="icon"
+            sizes="16x16"
+            type="image/png"
+          />
         </Head>
         <body className="bg-neutral-100 text-neutral-900 dark:text-neutral-100 dark:bg-neutral-900 transition-all">
           <Main />
